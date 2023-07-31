725cdefdb6f2e6811c10ff9c8e7cc3a3632c9b34
misaki <597234159@qq.com>
2023-07-31 08:17:53 +0800

    update wiki

---
+++ utils/init_price.py
@@ -0,0 +1,22 @@
+import base
+
+from web import models
+
+
+def run():
+    # 添加一个条目
+    exist = models.PricePolicy.objects.filter(category=1, title='个人免费版').exists()
+    if not exist:
+        models.PricePolicy.objects.create(
+            category=1, 
+            title='个人免费版',
+            price=0,
+            project_num=3,
+            project_member=2,
+            project_space=20,
+            per_file_size=5
+        )
+
+
+if __name__ == '__main__':
+    run()
--- web/forms/bootstrap.py
+++ web/forms/bootstrap.py
@@ -7,10 +7,11 @@ Description:
 '''
 class BootstrapForm(object):
     # 为所有标签加上样式
+    bootstrap_class_exclude = []
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for name, field in self.fields.items():
-            if name in self.bootstrap_class_exclude:
+            if self.bootstrap_class_exclude and (name in self.bootstrap_class_exclude):
                 continue
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = '请输入{0}'.format(field.label)
